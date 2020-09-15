d7d91b1ef59e93c6221eb79be007aa7c01396ce2
mengxuhui214175 <mengxuhui214175@sogou-inc.com>
2020-09-15 17:15:22 +0800

    注入测试

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
  * @@Description: web服务
  * @Author: i.mengxh@gmail.com
  * @Date: 2020-09-15 17:09:34
- * @LastEditTime: 2020-09-15 17:11:20
+ * @LastEditTime: 2020-09-15 17:14:54
  * @LastEditors: i.mengxh@gmail.com
  */
 const Koa = require('koa');
@@ -10,12 +10,11 @@ const Router = require('koa-router');
 const next = require('next');
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
-const handle = app.getRequestHandler();
+// const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
     const server = new Koa();
     const router = new Router();
-    console.log(demo - Bug);
     router.get(['/', '/index'], async (ctx) => {
         let indexPath = "/index";
 
